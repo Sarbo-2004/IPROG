0ddd48b216fd5ab907018db31a67253ec1e8391c
SARBO_2004 <86057572+Sarbo-2004@users.noreply.github.com>
2023-02-25 11:08:32 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 char team1,team2,t_won,t_choose;
 void member_input()
 {
-    printf("%s %s",team1,team2);
+    //printf("%s %s",team1,team2);
 }
 void input()
 {
@@ -14,7 +14,7 @@ void input()
     scanf("%s",team1);
     scanf("%s",team2);
     printf("Enter the number of overs:-");
-    scanf("%d",overs);
+    scanf("%d",&overs);
     printf("Who won the toss??\n");
     scanf("%s",t_won);
     printf("What did %s chose to do first?(batting/fielding)\n",t_won);
