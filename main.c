a5d08bdef9f70fe291b857a583ad190e7336723f
RudranotOP <126277385+RudranotOP@users.noreply.github.com>
2023-02-25 11:49:21 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,52 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-char team1,team2,t_won,t_choose;
+char team1[50],team2[50],t_won[50],t_choose[50],player[50];
 void member_input()
 {
-    printf();
+    FILE *fp, *fp1;
+    fp=fopen(team1,"w");
+      if (fp == NULL)
+    {
+        printf("Error opening file!\n");
+        //return 1;
+    }
     
+    for (int i = 1; i <= 11; i++) 
+    {
+        printf("enter player %d",i);
+        scanf("%s",player);
+        fprintf(fp, "%s\n", player);
+    }
+    
+    
+    fclose(fp);
+    fp1=fopen(team2,"w");
+      if (fp1 == NULL)
+    {
+        printf("Error opening file!\n");
+        //return 1;
+    }
+    
+    for (int j= 1; j <= 11; j++) 
+    {
+        printf("enter player %d",j);
+        scanf("%s",player);
+        fprintf(fp1, "%s\n", player);
+    }
+    
+    
+    fclose(fp1);
 }
+
 void input()
 {
-    // char team1,team2,t_won,t_choose;
+    
     int overs;
     printf("Enter names of two teams\n");
     scanf("%s",team1);
     scanf("%s",team2);
-    printf("Enter the number of overs:-");
+    printf("Enter the number of overs:-\n");
     scanf("%d",&overs);
     printf("Who won the toss??\n");
     scanf("%s",t_won);
@@ -27,4 +59,4 @@ void input()
 int main()
 {
    input();
-}                                                                                                            
+}    
